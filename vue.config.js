95dc4b7520d08502a79d35888bc2916756fd5370
Sotatek-DuongDo <duong.do@sotatek.com>
2022-05-20 06:29:02 +0000

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === 'production'
-      ? '/betu-tool/' // Thay tên repository của các bạn vào đây nhé
-      : '/',
-};
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/docs/'
+    : '/'
+}
