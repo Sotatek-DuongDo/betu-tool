68518558ad0b06b2a569bec5b5b518e8f305bb45
Sotatek-DuongDo <duong.do@sotatek.com>
2022-05-20 06:55:38 +0000

    deploy

---
+++ dist/dist/server/client.manifest.mjs
@@ -0,0 +1,21 @@
+export default {
+  "node_modules/nuxt/dist/app/entry.mjs": {
+    "file": "entry-55fee384.mjs",
+    "src": "node_modules/nuxt/dist/app/entry.mjs",
+    "isEntry": true,
+    "dynamicImports": [
+      "pages/index.vue"
+    ],
+    "css": [
+      "entry.df748495.css"
+    ]
+  },
+  "pages/index.vue": {
+    "file": "index-9e263989.mjs",
+    "src": "pages/index.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.mjs"
+    ]
+  }
+}
